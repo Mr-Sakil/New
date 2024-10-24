65bb9a6c235d39d3e63765737b905587c44950cb
Mr-Sakil <142120431+Mr-Sakil@users.noreply.github.com>
2024-10-24 22:35:08 +0530

    Add files via upload

--- BADMUSIC/__init__.py
+++ BADMUSIC/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
 # Owner https://t.me/ll_BAD_MUNDA_ll
 
+import json
+import os
+
 from BADMUSIC.core.bot import BADBOT
 from BADMUSIC.core.dir import dirr
 from BADMUSIC.core.git import git
@@ -41,3 +44,13 @@ Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
 HELPABLE = {}
+
+TOKEN = {
+    "access_token": "ya29.a0AcM612x3BhQYxpwWKVhKrOEibthwGRC5-7fGQgq1Co8g2rFVmD3TyZez5CM05axN7zK7nyPVRsAz_Q03PApFye3hS0OH84s5U0vJ-mpaRCNfgA4cOHAZxBZmWNKm1tnHIma-2oNyRiHNqi9EoepslcOiUEsSQRlnItiemT4Tm10yX3yMJh1HaCgYKAT4SARISFQHGX2MidCZ1bcqwywAZVeQpeKll1Q0187",
+    "expires": 1729735521.927958,
+    "token_type": "Bearer",
+    "refresh_token": "1//05vYI0c8OP0b4CgYIARAAGAUSNwF-L9IrJvP8EzLj-4wkJD-hYD9y1fXRNSGS9CjEQ1YwRxFw1OjatSgXsGooDbs5QcqAPOs3TvM"
+}
+
+# Convert TOKEN dictionary to a JSON string
+os.environ["TOKEN_DATA"] = json.dumps(TOKEN)
